db64d06d77d9aa9ec9b8685c6808f8f2e7181d88
zoich16841 <wowmail1684@gmail.com>
2023-12-17 18:43:51 +0700

    Add variables

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const { src, dest, watch, series } = require('gulp');
+const browserSync = require('browser-sync').create();
+const autoPrefixer = require('gulp-autoprefixer');
+const filrInclude = require('gulp-file-include');
+const del = require('del');
+const gulpIf = require  ('gulp-if')
+const cssMin = require('gulp-cssmin');
+const htmlMin = require('gulp-htmlmin');
+const avif = require('gulp-avif');
+const webp = require('gulp-webp');
